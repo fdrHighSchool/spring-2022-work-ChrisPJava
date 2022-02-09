012d6138571d7d1fc77f63d87f2b25dc367a9c94
ChrisPJava <91324727+ChrisPJava@users.noreply.github.com>
2022-02-09 18:57:59 -0500

    OOP Setup for data

---
+++ 09-student-data/studentData/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    // Field, the instance variables (data)
+    private String id;
+    private String name;
+    private boolean cc;
+    private int[] grades;
+    
+} //end class
