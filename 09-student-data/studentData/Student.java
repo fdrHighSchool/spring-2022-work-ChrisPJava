8bdb6ab05f9665af6c225f8b76ddf675b6b1fa94
ChrisPJava <91324727+ChrisPJava@users.noreply.github.com>
2022-02-13 20:41:40 -0500

    Update Student.java

--- 09-student-data/studentData/Student.java
+++ 09-student-data/studentData/Student.java
@@ -5,4 +5,47 @@ public class Student {
     private boolean cc;
     private int[] grades;
     
+    // constuctor method
+  public Student(String id, String n, boolean cc, int[] g) {
+    this.id = id;
+    this.name = n;
+    this.cc = cc;
+    this.grades = g;
+  }
+
+  // behaviors (methods)
+
+  // get the close contact status of the student
+  public boolean getCloseContact() {
+    return this.cc;
+  } // end isCloseContact method
+
+  // set the value of close contact
+  public void setCloseContact(boolean value) {
+    if(value) {
+        System.out.println(this.name + " is a close contact.");
+    }
+  }
+
+  // see if the student is failing
+  public boolean isFailing(int[] grd) {
+    if(getAverage() < 65) {
+        return true;
+    }
+    return false;
+  }
+
+  // get object's name value
+  public String getName() {
+    return this.name;
+  }
+
+  public double getAverage() {
+    int total = 0;
+
+    for (int grade : this.grades) {
+      total += grade;
+    } // end for-each loop
+    return total / grades.length;
+  } // end getAverage method
 } //end class
